41528e11bef09e0fb4641d3cd9349e98cedab9dc
enokaeru <enokaeru@gmail.com>
2019-06-12 09:20:57 +0900

    fix typo

--- dqn_agent.py
+++ dqn_agent.py
@@ -18,7 +18,7 @@ TAU = 1e-3
 LR = 5e-4
 UPDATE_EVERY = 4
 
-device = torch.device("cuda:0" if torch.cuda.is_avalibale() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class Agent():
